44a92b54488f651d111b283fc864e83990431b43
Theus16 <mateus.gari1b2@gmail.com>
2024-03-14 10:33:17 -0300

    Novo Botão

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Mateus</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Colne do projeto aula 14-03-2024 3B1 lado B</Text>
       <Text></Text>
       <StatusBar style="auto" />
+      <Button title='salvar'></Button>
     </View>
   );
 }
